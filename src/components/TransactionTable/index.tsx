57d8e4b76cfdd3f0736c512011ce519ad9a5af3a
Victor <victor.h.m.alves@gmail.com>
2021-03-18 02:55:05 +0100

    Using mirageJS to us list

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./style";
 
+interface Transaction{
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionTable () {
+const [transactions, setTransactions] = useState<Transaction[]>([]);
+
 useEffect(() => {
   api.get('transactions')
-  .then(response => console.log(response.data))
+  .then(response => setTransactions(response.data.transactions))
 }, []);
 
   return(
@@ -21,19 +32,24 @@ useEffect(() => {
         </thead>
 
         <tbody>
-        <tr>
-           <td>Webdesign</td>
-           <td className="deposit">€12.000</td>
-           <td>Design</td>
-           <td>02/01/2021</td>
-          </tr>
-
-          <tr>
-           <td>Rent</td>
-           <td className="withdraw">- €1.000</td>
-           <td>Home</td>
-           <td>31/01/2021</td>
-          </tr>
+          { transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {//new Intl.DateTimeFormat().resolvedOptions().timeZone
+                  new Intl.NumberFormat('es-ES', {
+                    style: 'currency',
+                    currency: 'EUR'
+                  }).format(transaction.amount)
+                }
+              </td>
+              <td>{transaction.category}</td>
+              <td>{
+               new Intl.DateTimeFormat('es-ES').format(new Date(transaction.createdAt))
+              }</td>
+            </tr>
+          ))
+          }
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      // sempre é o model no plural
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance',
+          type: 'deposit',
+          category: 'Development',
+          amount: 1500,
+          createdAt: new Date('2021-03-18'),
+        },
+        {
+          id: 2,
+          title: 'Rent',
+          type: 'withdraw',
+          category: 'Home',
+          amount: 700,
+          createdAt: new Date('2021-03-20'),
+        }
+      ]
+    })
+  },
+
   routes() {
     // Buscar tudo após api
     this.namespace = 'api';
 
     //buscar tudo para a rota...
     this.get('/transactions', () => {
-      return [
-        {
-          id:1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt:new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
